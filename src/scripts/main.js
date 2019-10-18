e4e1272642f9f3108189ec337e04018bdf9d34c9
Ryan Bishop <rdbishop06@gmail.com>
2019-10-18 15:17:16 -0500

    practice: pet factory function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,40 +1,40 @@
-// console.log("I've been to Reno, Chicago, Fargo, Minnesota");
+console.log("I've been to Reno, Chicago, Fargo, Minnesota");
 
-// const metals = [
-//     { metal: "Sterling Silver", price: 12.42 },
-//     { metal: "14K Gold", price: 736.4 },
-//     { metal: "24K Gold", price: 1258.9 },
-//     { metal: "Platinum", price: 795.45 },
-//     { metal: "Platinum", price: 1241.0 }
-// ]
+const metals = [
+    { metal: "Sterling Silver", price: 12.42 },
+    { metal: "14K Gold", price: 736.4 },
+    { metal: "24K Gold", price: 1258.9 },
+    { metal: "Platinum", price: 795.45 },
+    { metal: "Platinum", price: 1241.0 }
+]
 
-// const diamonds = [
-//     { carets: 0.5, price: 405 },
-//     { carets: 0.75, price: 782 },
-//     { carets: 1, price: 1470 },
-//     { carets: 1.5, price: 1997 },
-//     { carets: 2, price: 3638 }
-// ]
+const diamonds = [
+    { carets: 0.5, price: 405 },
+    { carets: 0.75, price: 782 },
+    { carets: 1, price: 1470 },
+    { carets: 1.5, price: 1997 },
+    { carets: 2, price: 3638 }
+]
 
-// const ringStyles = [
-//     { style: "Classic", price: 500 },
-//     { style: "Modern", price: 710 },
-//     { style: "Vintage", price: 965 }
-// ]
+const ringStyles = [
+    { style: "Classic", price: 500 },
+    { style: "Modern", price: 710 },
+    { style: "Vintage", price: 965 }
+]
 
-// const createCustomRing = (style, carets, metal) => {
-//     return {
-//         style: style.style,
-//         carets: carets.carets,
-//         metal: metals.metal,
-//         price: metal.price + style.price + carets.price
-//     }
-// }
+const createCustomRing = (style, carets, metal) => {
+    return {
+        style: style.style,
+        carets: carets.carets,
+        metal: metals.metal,
+        price: metal.price + style.price + carets.price
+    }
+}
 
-// const newRingOrder = createCustomRing(ringStyles[1], diamonds[2], metals[2])
-// // Then add order to customer's shopping cart
+const newRingOrder = createCustomRing(ringStyles[1], diamonds[2], metals[2])
+// Then add order to customer's shopping cart
 
-// console.log(newRingOrder);
+console.log(newRingOrder);
 
 /* 
 Lightning Exercise 1: Write a factory function that creates an object that represents a doctor. 
@@ -62,3 +62,17 @@ Pet name
 Pet breed
 Invoke the factory function 3 times and place each animal in an array stored in a variable named BowWowKennels
  */
+
+ const newPet = (name, breed) => {
+     return {
+         name: name,
+         breed: breed
+     }
+ }
+
+ const maggie = newPet("Maggie", "Calico");
+ const fido = newPet("Fido", "Beagle");
+ const fluffy = newPet("Fluffy", "Dragon");
+
+ const BowWowKennels = [maggie, fido, fluffy];
+ console.log(maggie);
